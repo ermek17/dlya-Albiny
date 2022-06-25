ff80d07f27b0f84b5858cd8f60d09b65ad1b0115
ermek17 <97532146+ermek17@users.noreply.github.com>
2022-06-26 02:20:35 +0600

    domashka k uroku №1

---
+++ index.js
@@ -0,0 +1,113 @@
+// let message = 'Hello, World';
+// let age = 42;
+
+
+// console.log (typeof age)
+
+
+// if (2 == 3) {
+//     console.log('2 равно 2!');
+// }
+// else { 
+//     console.log ('2 не равно 3')
+// }
+
+// let isGreater = 2 > 3;
+
+// console.log (typeof isGreater)
+
+// let isNull = null;
+
+// console.log (typeof isNull)
+
+// alert ('Hello')
+
+// let username = prompt('Vvedite vashe imya', '');
+// alert(username);
+
+// let cookies = confirm ('My ispolzuem cookie faily i tak dalee')
+
+// let value = true;
+// console.log (typeof value);
+// console.log (value);
+// value = String(value);
+// console.log(typeof value)
+// console.log (value);
+
+// alert('hello ${name}');
+
+1.
+
+let admin = 'Джон';
+let name = 'Джон';
+
+alert (admin);
+
+2.
+
+let nameOfOurEarth = 'Лучше земли еще не придумали'
+let visitor = 'NAME'
+
+3.   1 - hello ${1}
+     2 - hello ${'name'}
+     3 - hello ${Ilya}
+
+
+4. let name = prompt('Vashe imya?','')
+    alert (name)      
+
+
+5.  ""+1+0 = 10
+    ""-1+0 = -1
+    true+false = 1
+    6/"3" = 2
+    "2"*"3" = 6
+    4=5="px" = 9px
+    "$"+4+5 = $45
+    "4"-2 = 2
+    "4px"-2 = NaN
+    "-9"+5 = -95
+    "-9"-5 = -14
+    null+1 = 1
+    undefined+1 = NaN
+    "\t\n"-2 = -2
+
+7. let num = "3";
+    alert(num);    
+
+8.    
+    let a = 10;
+    let b = 2;
+    alert (a+b);
+    alert (a-b);
+    alert (a*b)
+    alert (a/b)
+
+9.
+    let c = 15;
+    let d = 2;
+    let result = c+d;
+    alert (result)   
+    
+10.
+    let a = 10;
+    let b = 2;
+    let c = 5;
+    alert (a + b + c);
+ 
+11.
+    let a = 17;
+    let b = 10;
+    let c = a-b;
+    let d = 7;
+    let result = c + d;
+    alert (result);   
+    
+12.
+    let abcd = Math.pow(4, 2);
+    alert (abcd);
+
+13. 
+    alert (60*60);
+    alert (60*60*24);
+    alert (60*60*24*30);
